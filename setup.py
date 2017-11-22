bd96c8affb5f4e5cbf5c6349db0649257b14ea0b
fabregas <kksstt@gmail.com>
2017-11-22 10:36:42 +0200

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
 
 setup(
     name="aiokafka_rpc",
-    version="1.2.0",
+    version="1.2.1",
     author='Kostiantyn Andrusenko',
     author_email='kksstt@gmail.com',
     description=("RPC over Apache Kafka for Python using asyncio"),
